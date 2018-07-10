5ccdc002362ce584fdcee063946fcf8ce9f28fd7
Mercedes Vetráb <vetrab.mercedes@sonrisa.hu>
2018-07-10 14:15:45 +0200

    working libsvm example

--- startClass.py
+++ startClass.py
@@ -1,13 +1,15 @@
 import sys
-sys.path.append('.\libsvm\python')
+sys.path.append('.\libsvm-master\python')
 
 from svmutil import *
 
-y, x = [1,-1], [[1,0,1], [-1,0,-1]];
-
-prob = svm_problem(x,y);
-param = svm_parameter(kernel_type = LINEAR, C = 10);
-## training  the model
-m = svm_model(prob, param);
-#testing the model
-m.predict([1, 1, 1]);
+# Precomputed kernel data (-t 4)
+# Dense data
+y, x = [1,-1], [[1, 2, -2], [2, -2, 2]]
+# Sparse data
+y, x = [1,-1], [{0:1, 1:2, 2:-2}, {0:2, 1:-2, 2:2}]
+# isKernel=True must be set for precomputer kernel
+prob  = svm_problem(y, x, isKernel=True)
+param = svm_parameter('-t 4 -c 4 -b 1')
+m = svm_train(prob, param)
+# For the format of precomputed kernel, please read LIBSVM README.
